f74ab1d5bea82c02539ee325b1479544736716db
Eruda <957851968@qq.com>
2017-11-22 22:01:03 +0800

    animation v1.1 //really animation

--- JavascriptDomAnimation/animationJS/animation.js
+++ JavascriptDomAnimation/animationJS/animation.js
@@ -3,14 +3,34 @@
  */
 function positionMove(){
     var moveMassage=document.getElementById("moveMassage");
-    moveMassage.style.position="absolute";
-    moveMassage.style.left="50px";
-    moveMassage.style.top="100px";
-    setTimeout("chengePosition()",5000);
+    var xpos=parseInt(moveMassage.style.left);
+    var ypos=parseInt(moveMassage.style.top);
+    if(xpos!=1000||ypos!=400){
+        if(xpos<1000){
+            xpos++;
+        }
+        if(xpos>1000){
+            xpos--;
+        }
+        if(ypos<400){
+            ypos++;
+        }
+        if(ypos>400){
+            ypos--;
+        }
+    }else {
+        return true;
+    }
+    moveMassage.style.left=xpos+"px";
+    moveMassage.style.top=ypos+"px";
+    setTimeout("positionMove()",10);
 }
 
 function chengePosition() {
     var moveMassage=document.getElementById("moveMassage");
-    moveMassage.style.left="1000px";
+    moveMassage.style.position="absolute";
+    moveMassage.style.left="50px";
+    moveMassage.style.top="100px";;
 }
+addLoadEvent(chengePosition);
 addLoadEvent(positionMove);
