2784fbba890cae971e9418bb0945880925108b70
ItouMakoto <18302880084@163.com>
2017-11-25 16:03:59 +0800

    where and how to use animation

--- JavascriptDomAnimation/animationJS/positionMove.js
+++ JavascriptDomAnimation/animationJS/positionMove.js
@@ -2,8 +2,18 @@
  * Created by Eruda on 2017/11/23.
  */
 function positionMove(elementID,finalX,finalY,interval){//对函数进行抽象  移动对象/最终x位置/最终y位置/以及每次移动的距离
-
+    if(!document.getElementById)//测试浏览器兼容性
+    {
+        console.log("getElementbyid is not siutble");
+        return false;
+    }
+    if(!document.getElementById(elementID)){//测试是否有当前元素
+        console.log("can't catch the element");
+        return false;
+    }
     var element=document.getElementById(elementID);
+
+    if(!element.style.left||!element.style.top) return false;
     var xpos=parseInt(element.style.left);
     var ypos=parseInt(element.style.top);
     if(xpos!=finalX||ypos!=finalY){
@@ -24,5 +34,7 @@ function positionMove(elementID,finalX,finalY,interval){//对函数进行抽象 
     }
     element.style.left=xpos+"px";
     element.style.top=ypos+"px";
-    setTimeout("positionMove('"+elementID+"','"+finalX+"','"+finalY+"','"+interval+"')",interval);//这里的函数传递应为需要拼接字符串，函数名称中带参数引号，需要用单引号嵌套 用加号表示参数
+    //这里的函数传递应为需要拼接字符串，函数名称中带参数引号，需要用单引号嵌套 用加号表示参数
+    setTimeout("positionMove('"+elementID+"','"+finalX+"','"+finalY+"','"+interval+"')",interval);
+
 }
